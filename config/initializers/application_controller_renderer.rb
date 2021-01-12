38fdbd67168dbfb898508de4b6e2442130e848be
EC2 Default User <GitHub>
2021-01-12 13:54:02 +0000

    Meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7ce47b5893ed775b55fc7d5afda605ff9ff26b1a68718ca2654477f333c3b0e4d64619d7d9ff0f0de1e6d497de5791f760020a13dfa44c01cf430ccea6b1e7cb'
